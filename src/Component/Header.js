e785604f3601a181168f5db60564062b65f961d6
ahmadezzat472 <admedo472@gmail.com>
2024-04-28 17:48:55 +0200

    header

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Header.css'
 import logo from './img/logo.png';
-import img from './img/img.png';
+import img from './img/img3.png';
 
 function Header() {
     return (
         <div className='header'>
             <img className='logo' src={logo} alt='the logo' />
-            <p className='title'>  اختبار اضطرابات الأكل </p>
+            <p className='title'>  اختبار اضطرابات الطعام </p>
             <img className='img' src={img} alt='img' />
         </div>
     )
